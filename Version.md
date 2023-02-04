20faa945f59e25bdd4f1a440dd740c476361feff
Serafirim <serafirim@gmail.com>
2023-02-04 11:20:39 -0600

    push 1.0.1

--- Version.md
+++ Version.md
@@ -6,9 +6,13 @@
 ### 3rd             Change - Minor updates and hotfixes
 
 ## Current Version
-1.0.0
+1.0.1
 
 ## Version
 	2023-02-01		Version 1.0.0
 
 				The code is born!
+
+	2023-02-04		Version 1.0.1
+
+				Continued learning the tutorial
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,42 +20,58 @@ function App() {
     // simulate a API environment
     setTimeout(() => {
       setSubmitting(false)
-    }, 3000)
+    }, 5000)
   }
 
   const handleChange = event => {
+    const isCheckbox = event.target.type === 'checkbox'
+
     setFormData({
       name: event.target.name,
-      value: event.target.value,
+      value: isCheckbox ? event.target.checked : event.target.value,
     })
   }
 
 
   return(
     <div className="wrapper">
-      <h1>And that's the waaaaaaaay the news goes!</h1>
-
-      {
-        submitting && 
+      <h1>How About Them Apples</h1>
+      {submitting &&
         <div>
           You are submitting the following:
           <ul>
-            {
-              Object.entries(formData).map(([name, value]) => (
-                <li key={name}><strong>{name}</strong>:{value.toString()}</li>
-              ))
-            }
-          </ul>  
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+            ))}
+          </ul>
         </div>
       }
-
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Name</p>
-            <input name="name" onChange={handleChange} />
+            <input name="name" onChange={handleChange}/>
           </label>
         </fieldset>
+        <fieldset>
+         <label>
+           <p>Apples</p>
+           <select name="apple" onChange={handleChange}>
+               <option value="">--Please choose an option--</option>
+               <option value="fuji">Fuji</option>
+               <option value="jonathan">Jonathan</option>
+               <option value="honey-crisp">Honey Crisp</option>
+           </select>
+         </label>
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" onChange={handleChange} step="1"/>
+         </label>
+         <label>
+           <p>Gift Wrap</p>
+           <input type="checkbox" name="gift-wrap" onChange={handleChange} />
+         </label>
+       </fieldset>
         <button type="submit">Submit</button>
       </form>
     </div>
